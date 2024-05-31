01bda7862052895f694d974a4e31440b38b2f2fe
김진영 <>
2024-05-31 17:22:55 +0900

    feat : 바텀 내비게이션 스타일 지정

--- daangn/app/src/main/java/com/project/daangn/MainActivity.kt
+++ daangn/app/src/main/java/com/project/daangn/MainActivity.kt
@@ -18,11 +18,6 @@ class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::infl
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
         navController = navHostFragment.findNavController()
