9566b4d21bcfa28560a06750f58e9ab550596e67
김진영 <>
2024-05-31 15:45:28 +0900

    feat : 바텀 내비게이션 연결

---
+++ daangn/app/src/main/java/com/project/daangn/util/base/BaseActivity.kt
@@ -0,0 +1,23 @@
+package com.project.daangn.util.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewbinding.ViewBinding
+
+abstract class BaseActivity<B : ViewBinding>(private val inflate: (LayoutInflater) -> B) :
+    AppCompatActivity() {
+    protected lateinit var binding: B
+        private set
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+}
