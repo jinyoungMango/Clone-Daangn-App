6920cd1ad6b3df1be0831799dca81b497a4481ba
김진영 <>
2024-05-31 18:16:50 +0900

    feat : toolbar 설정

--- daangn/app/src/main/java/com/project/daangn/MainActivity.kt
+++ daangn/app/src/main/java/com/project/daangn/MainActivity.kt
@@ -18,9 +18,15 @@ class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::infl
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0)
+            insets
+        }
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
         navController = navHostFragment.findNavController()
         binding.bottomNavigationView.setupWithNavController(navController)
+        binding.topAppBar.setupWithNavController(navController)
     }
 }
